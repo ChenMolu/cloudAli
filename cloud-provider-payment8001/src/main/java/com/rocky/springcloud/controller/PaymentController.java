c7336a324c95eedec1f6f18e674d2604d4ad8978
Rocky <993916778@qq.com>
2023-09-25 10:14:42 +0800

    feat:消费者订单实现&&项目重构抽取api-common层

--- cloud-provider-payment8001/src/main/java/com/rocky/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/rocky/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.rocky.springcloud.entities.CommonResult;
 import com.rocky.springcloud.entities.Payment;
 import com.rocky.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult createPayment(Payment payment) {
+    public CommonResult createPayment(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         if (result > 0) {
             return new CommonResult(200, "success", result);
